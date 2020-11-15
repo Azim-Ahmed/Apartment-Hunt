16bf5be5dde1ed99327bba470179a46586e1ebe9
sufian111 <67520097+sufian111@users.noreply.github.com>
2020-11-15 18:57:05 +0600

    check is it ok or not ?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       react router
+      <p>sufian</p>
     </div>
   );
 }
