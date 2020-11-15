b0ea5ab8b7901b5ee977491fc7e7a50aad98b4e2
sufian111 <67520097+sufian111@users.noreply.github.com>
2020-11-15 22:13:24 +0600

    navbar

--- src/Components/Home/HouseRent/HouseRent.js
+++ src/Components/Home/HouseRent/HouseRent.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import fakedata from '../../../../src/FakeData/Fakedata'
+import React from "react";
+import fakedata from "../../../../src/FakeData/Fakedata";
 
 const HouseRent = () => {
-
-    
-    return (
-        <div >
-
-
-<div className="text-center text-info">
-<p>House Rent</p>
-        <h2>Discover The Latest Rent <br/> Available Today</h2>
-        <h2>Discover The Latest Rent <br/> Available Today</h2>
-        
-</div>
-{/* <div>
+  return (
+    <div>
+      <div className="text-center text-info">
+        <p>House Rent</p>
+        <h2>
+          Discover The Latest Rent <br /> Available Today
+        </h2>
+        <h2>
+          Discover The Latest Rent <br /> Available Today
+        </h2>
+      </div>
+      {/* <div>
     {fakedata.map(item=> <li>{item.title}</li> )}
 </div> */}
 
-<div className = "d-flex  justify-content-center">
-          <div className='row w-75 mt-5 pt-5 '>
-                {
+      <div className="d-flex  justify-content-center">
+        <div className="row w-75 mt-5 pt-5 ">
+          {/* {
                   fakedata.length ?  fakedata.map((service, i) =>
                         
                         <>
@@ -29,18 +28,13 @@ const HouseRent = () => {
                             
                             <img className = 'mb-3 mt-3' style ={{height:"70px"}}  src={`data:image/png;base64,${service.img.img}`}alt="" />
                             <h5>{service.name}</h5>
-                <p className = "text-secondary">{service.desc}</p>
+                     <p className = "text-secondary">{service.desc}</p>
                 </Link>
-                 </>
-}
-                 </div>
-                 </div>
-
-                
-               
-
+                 </> */}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HouseRent;
